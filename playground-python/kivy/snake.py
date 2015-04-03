1c3e3fe1c2dea8f06315892c283243f1c7dbca12
fatcloud <victor19871226@hotmail.com>
2015-04-04 05:20:17 +0800

    Try things out
    Now we know the size of a widget is set after build() of an App is called

--- playground-python/kivy/snake.py
+++ playground-python/kivy/snake.py
@@ -11,40 +11,40 @@ from kivy.graphics import Color, Rectangle
 from random import random as r
 from functools import partial
     
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
 
-class BoxesCanvus(BoxLayout):
+class BoxesCanvus(Widget):
 
     def __init__(self, **kwargs):
         super(BoxesCanvus, self).__init__(**kwargs)
         self.cols = 1
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self._keyboard.bind(on_key_down=self._on_key_down)
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
         
 
-    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        print 'hahaha'
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        self.add_rects(100)
         
     def add_rects(self, count, *largs):
         with self.canvas:
             for x in range(count):
                 Color(r(), 1, 1, mode='hsv')
-                Rectangle(pos=(r() * self.width + 100,
-                               r() * self.height + 100), size=(20, 20))
+                Rectangle(pos=(r() * self.width,
+                               r() * self.height), size=(20, 20))
 
 class MyApp(App):
 
     def build(self):
         bc = BoxesCanvus()
-        bc.add_rects(100)
         return bc
+
+    def on_start(self):
+        self.root.add_rects(100)
 
 
 if __name__ == '__main__':

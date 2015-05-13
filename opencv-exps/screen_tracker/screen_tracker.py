c732cf6ba1921828849cdbb5c2533fd4f10f1e1c
fatcloud <victor19871226@hotmail.com>
2015-05-13 14:17:49 +0800

    findpolygon seems to go down. must debug today.

--- opencv-exps/screen_tracker/screen_tracker.py
+++ opencv-exps/screen_tracker/screen_tracker.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 from motion_detect import MotionDetector
 from cam import MyCam
-
-# cam = MyCam()
+from find_square_code import find_polygons
 
 if __name__ == '__main__':
     img = np.full((600,800,3), 0, np.uint8)
@@ -29,12 +28,12 @@ if __name__ == '__main__':
         cam_img = cam.read()
         md.feed_image(cam_img)
         gray_diff = cv2.cvtColor(md.diff, cv2.COLOR_BGR2GRAY)
-        blured = cv2.blur(gray_diff, (5,5))
+        # blured = cv2.blur(gray_diff, (5,5))
         
-        # edges = cv2.Canny(gray_diff, 100, 200)
-        _, th = cv2.threshold(blured, 20, 255, cv2.THRESH_BINARY)
+        # # edges = cv2.Canny(gray_diff, 100, 200)
+        # _, th = cv2.threshold(gray_diff, 80, 255, cv2.THRESH_BINARY)
         
-        contours, hry = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # contours, hry = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
         # screens = []
         # for ctr in contours:
@@ -44,11 +43,14 @@ if __name__ == '__main__':
             # if len(tmp) == 4 and cv2.contourArea(tmp) > 1000:
                 # screens.append(tmp)
         
-        # for tmp in screens:
-        cv2.drawContours(th, contours, 0, (0, 0, 255))
-        print contours
-        cv2.imshow('motion', th)
+        quadrangles = find_polygons(cam_img, 4, 0.5, 1000)
+        # th = cv2.cvtColor(gray_diff, cv2.COLOR_GRAY2BGR)
+        print quadrangles
+        for ctr in quadrangles:
+            cv2.drawContours(cam_img, [ctr], 0, (0, 0, 255), 3)
+        
+        cv2.imshow('motion', cam_img)
         cv2.imshow('BGR', img)
-        k = cv2.waitKey(500)
+        k = cv2.waitKey(300)
         if k == 27:
             break
